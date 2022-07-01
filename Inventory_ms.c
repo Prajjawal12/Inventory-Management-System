a0198f7b629a3828ffd2a81d294f078ea5abfa6c
Prajjawal12 <99403378+Prajjawal12@users.noreply.github.com>
2022-07-01 15:16:44 +0530

    Add files via upload

---
+++ Inventory_ms.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+bool checkPassword(password == 'password123');
+{
+    if (password == '')
+        return true;
+    return false;
+}
+int main()
+{
+    char password[] = 'password123';
+    scanf("%d", &password);
+    if (!checkPassword(password))
+        return;
+    int choice;
+    printf("*\t*\t*\t*\t*****************AMURoboclub Inventory Management System********************\t*\t*\t*\t\n");
+    printf("\n\n\n\n\n\n\n\n\n\n");
+
+    printf("Operations That can be performed are as follows:\n\n\n\n\n\n\n");
+    printf("Choose the number corresponding to your desired operation\n\n\n");
+
+    /* printf("1.)Add an item in the inventory.\n\n\n\n");
+     printf("2.)Delete an item from the inventory.\n\n\n\n");
+     printf("3.)Issue an Item from the inventory.\n\n\n\n");*/
+    printf("1.)Return the item to an inventory.\n\n\n\n");
+    printf("2.)Show all the items in the inventory.\n\n\n\n");
+
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+        printf("")
+    }
+
+    return 0;
+}
